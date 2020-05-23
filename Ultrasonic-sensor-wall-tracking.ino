c18b0c93bae77fd0f0e712d8e0f41ac1eddfa76b
SRV1030 <65103790+SRV1030@users.noreply.github.com>
2020-05-23 23:03:36 +0545

    Update Ultrasonic-sensor-wall-tracking.ino

--- Ultrasonic-sensor-wall-tracking.ino
+++ Ultrasonic-sensor-wall-tracking.ino
@@ -9,30 +9,20 @@
 #define M1 6
 #define M2 9
 void f()
-{analogWrite(L1,153);
+{analogWrite(L1,162);
 analogWrite(L2,0);
 analogWrite(M1,0);
-analogWrite(M2,153);}
+analogWrite(M2,162);}
 void r()
-{analogWrite(L1,153);
+{analogWrite(L1,165);
 analogWrite(L2,0);
-analogWrite(M1,148);
-analogWrite(M2,0);}
-void rt()
-{analogWrite(L1,0);
-analogWrite(L2,0);
-analogWrite(M1,153);
+analogWrite(M1,165);
 analogWrite(M2,0);}
 void l()
 {analogWrite(L1,0);
-analogWrite(L2,148);
+analogWrite(L2,165);
 analogWrite(M1,0);
-analogWrite(M2,153);}
-void lt()
-{analogWrite(L1,0);
-analogWrite(L2,153);
-analogWrite(M1,0);
-analogWrite(M2,0);}
+analogWrite(M2,165);}
 void fw()
 {analogWrite(L1,255);
 analogWrite(L2,0);
@@ -40,8 +30,8 @@ analogWrite(M1,0);
 analogWrite(M2,255);}
 void bk()
 {analogWrite(L1,0);
-analogWrite(L2,153);
-analogWrite(M1,153);
+analogWrite(L2,165);
+analogWrite(M1,165);
 analogWrite(M2,0);}
 void setup()
 {Serial.begin(9600);
@@ -51,10 +41,12 @@ pinMode(t2, OUTPUT);
 pinMode(e2, INPUT);
 pinMode(t3, OUTPUT);
 pinMode(e3, INPUT);
+
 pinMode(L1, OUTPUT);
 pinMode(L2, OUTPUT);
 pinMode(M1, OUTPUT);
 pinMode(M2, OUTPUT);}
+
 void loop()
 {float d1,di1,d2,di2,d3,di3;
 digitalWrite(t1, HIGH);
@@ -75,50 +67,72 @@ digitalWrite(t3, HIGH);
 delay(100);
 digitalWrite(t3, LOW);
 d3=pulseIn(e3, HIGH);
-di3= (d3/ 2)*0.034;
+di3= (d3/2)*0.034;
 Serial.print(" di3: ");
 Serial.println(di3);
-if ( di1<11.5 && di2>=34.5 && di3>14) 
-{bk();
-delay(200);
-rt();
-delay(150);
-f();
-delay(200);
-fw();}
-else if ( di1>14 && di2>=34.5 && di3<11.5) 
-{bk();
-delay(200);
-lt();
-delay(150);
-f();
-delay(200);
-fw();}
-else if ( di1<=14 && di2<34.5 && di3>14) 
-{bk();
-delay(200);
-r();
-delay(421);
-f();}
-else if ( di1>14 && di2<34.5 && di3<=14) 
+if ( di1<=18.2 && di2<22 && di3<=18.5) 
+{{ if (di1>di3)
 {bk();
 delay(200);
 l();
-delay(421);
-f();}
-else if ( di1>14 && di2<34.5 && di3>14) 
-{if (di1>di3)
-{bk();
-delay(200);
-l();
-delay(421);
+delay(180);
 f();}
 else if (di1<di3)
 {bk();
 delay(200);
 r();
-delay(421);
-f();;}}
+delay(180);
+f();}}}
+else if ( di1>18.2 && di2<22 && di3>18.5) 
+{{ if (di1>di3)
+{bk();
+delay(200);
+l();
+delay(180);
+f();}
+else if (di1<di3)
+{bk();
+delay(200);
+r();
+delay(180);
+f();}}}
+else if ( di1<10 && di2>=36 && di3>18.5) 
+{bk();
+delay(200);
+r();
+delay(160);
+f();}
+else if ( di1>18.2 && di2>=36 && di3<10.3) 
+{bk();
+delay(200);
+l();
+delay(160);
+f();}
+else if ( di1<=18.2 && di2<36 && di3>18.5) 
+{bk();
+delay(200);
+r();
+delay(250);
+f();}
+else if ( di1>18.2 && di2<36 && di3<=18.5) 
+{bk();
+delay(200);
+l();
+delay(250);
+f();}
+else if ( di1>18.2 && di2<36 && di3>18.5) 
+{ if (di1>di3)
+{bk();
+delay(200);
+l();
+delay(180);
+f();}
+else if (di1<di3)
+{bk();
+delay(200);
+r();
+delay(180);
+f();}}
 else
 {fw();}}
     
